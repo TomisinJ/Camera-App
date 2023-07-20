330bd23569676349310a35cf35ed61d26a5f9bf5
Tomisin <tomisin@tomisins-mbp.home>
2023-07-20 20:13:41 +0100

    Created camera component

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState , useEffect, useRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera , CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
+  // variables to keep track of the camera
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back); // set front or back camera
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off); // set flash
+  const cameraRef = useRef(null);
+
+  // this is creating/enabling the permission alert  
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted'); // user has granted permission === true
+    })();
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  <Camera
+    style={styles.camera}
+    type={type}
+    flashMode={flash}
+    ref={cameraRef}
+  >
+    <Text>hello</Text>
+  </Camera>
+  <View>
+    
+  </View>
     </View>
   );
 }
@@ -14,7 +40,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+
+  camera: {
+   flex: 1, // using the full space of the view
+   borderRadius: 50,
+  }
 });
